f1118b514b4f73290c45b44d89b6aca405d1b070
Dknangia <dknangia@gmail.com>
2021-12-07 09:40:52 -0500

    .

--- Food/Models/FoodModel.cs
+++ Food/Models/FoodModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +7,8 @@ namespace Food.Models
     public class FoodModel
     {
         public string URL { get; set; }
-        public int id { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
-
-        public double Price { get; set; }
+        public double Price { get; set; }        
     }
 }
