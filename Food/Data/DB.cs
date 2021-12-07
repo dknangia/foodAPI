191c344d43f6916b12b418668aced9717926a2f3
Dknangia <dknangia@gmail.com>
2021-12-07 09:40:31 -0500

    .

--- Food/Data/DB.cs
+++ Food/Data/DB.cs
@@ -24,14 +24,14 @@ namespace Food.Data
 
         List<FoodModel> Get()
         {
-            foodList.Add(new FoodModel { id = 1, Name = "Bread", Price = 1d  }) ;
-            foodList.Add(new FoodModel { id = 2, Name = "Rice", Price = 1.5d });
-            foodList.Add(new FoodModel { id = 3, Name = "Boiled Potato", Price = 0.80d });
-            foodList.Add(new FoodModel { id = 4, Name = "Pasta", Price = 5.69d });
-            foodList.Add(new FoodModel { id = 5, Name = "Cheese pasta", Price = 14.0d });
-            foodList.Add(new FoodModel { id = 6, Name = "Butter Bread", Price = 2.96d });
-            foodList.Add(new FoodModel { id = 7, Name = "Chocolate milk", Price = 7.84d });
-            foodList.Add(new FoodModel { id = 8, Name = "Salad", Price = 6.36d });
+            foodList.Add(new FoodModel { ID = 1, Name = "Bread", Price = 1d  }) ;
+            foodList.Add(new FoodModel { ID = 2, Name = "Rice", Price = 1.5d });
+            foodList.Add(new FoodModel { ID = 3, Name = "Boiled Potato", Price = 0.80d });
+            foodList.Add(new FoodModel { ID = 4, Name = "Pasta", Price = 5.69d });
+            foodList.Add(new FoodModel { ID = 5, Name = "Cheese pasta", Price = 14.0d });
+            foodList.Add(new FoodModel { ID = 6, Name = "Butter Bread", Price = 2.96d });
+            foodList.Add(new FoodModel { ID = 7, Name = "Chocolate milk", Price = 7.84d });
+            foodList.Add(new FoodModel { ID = 8, Name = "Salad", Price = 6.36d });
 
             return foodList;
         }
