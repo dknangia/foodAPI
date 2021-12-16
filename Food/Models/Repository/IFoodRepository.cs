86c2f6f7d1a72c2a553c2a221bb4b9daf4aae3fd
Dknangia <dknangia@gmail.com>
2021-12-16 14:55:55 -0500

    .

---
+++ Food/Models/Repository/IFoodRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Food.Models.Repository
+{
+    public interface IFoodRepository
+    {
+        IList<FoodModel> GetFood();
+        FoodModel GetFood(int foodId);
+
+        FoodModel SaveFood(FoodModel toSave);
+
+        void UpdateFood(FoodModel toUpdate); 
+    }
+}
