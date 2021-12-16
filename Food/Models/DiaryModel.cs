c73db282caa84356d28932106fb0782ccf29fda9
Dknangia <dknangia@gmail.com>
2021-12-15 23:22:23 -0500

    .

---
+++ Food/Models/DiaryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Food.Models
+{
+    public class DiaryModel
+    {
+        public int ID { get; set; }
+        public FoodModel FoodEntry { get; set; }
+        public DateTime currentDate { get; set; }
+        public string UserName { get; set; }
+    }
+}
