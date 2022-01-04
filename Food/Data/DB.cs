da33581595281de0a166fa4f3456b5bc23336347
Dknangia <dknangia@gmail.com>
2022-01-03 21:33:58 -0500

    no message

--- Food/Data/DB.cs
+++ Food/Data/DB.cs
@@ -8,32 +8,32 @@ using System.Web.Http.Routing;
 
 namespace Food.Data
 {
-    public class DB
+    public class Db
     {
-        public static List<FoodModel> foodList = null;
+        public static List<FoodModel> FoodList = null;
         
-        public DB()
+        public Db()
         {
             
-            if (foodList == null)
+            if (FoodList == null)
             {
-                foodList = new List<FoodModel>();
+                FoodList = new List<FoodModel>();
                 Get();
             }
         }
 
         List<FoodModel> Get()
         {
-            foodList.Add(new FoodModel { ID = 1, Name = "Bread", Price = 1d  }) ;
-            foodList.Add(new FoodModel { ID = 2, Name = "Rice", Price = 1.5d });
-            foodList.Add(new FoodModel { ID = 3, Name = "Boiled Potato", Price = 0.80d });
-            foodList.Add(new FoodModel { ID = 4, Name = "Pasta", Price = 5.69d });
-            foodList.Add(new FoodModel { ID = 5, Name = "Cheese pasta", Price = 14.0d });
-            foodList.Add(new FoodModel { ID = 6, Name = "Butter Bread", Price = 2.96d });
-            foodList.Add(new FoodModel { ID = 7, Name = "Chocolate milk", Price = 7.84d });
-            foodList.Add(new FoodModel { ID = 8, Name = "Salad", Price = 6.36d });
+            FoodList.Add(new FoodModel { Id = 1, Name = "Bread", Price = 1d  }) ;
+            FoodList.Add(new FoodModel { Id = 2, Name = "Rice", Price = 1.5d });
+            FoodList.Add(new FoodModel { Id = 3, Name = "Boiled Potato", Price = 0.80d });
+            FoodList.Add(new FoodModel { Id = 4, Name = "Pasta", Price = 5.69d });
+            FoodList.Add(new FoodModel { Id = 5, Name = "Cheese pasta", Price = 14.0d });
+            FoodList.Add(new FoodModel { Id = 6, Name = "Butter Bread", Price = 2.96d });
+            FoodList.Add(new FoodModel { Id = 7, Name = "Chocolate milk", Price = 7.84d });
+            FoodList.Add(new FoodModel { Id = 8, Name = "Salad", Price = 6.36d });
 
-            return foodList;
+            return FoodList;
         }
     }
 }
